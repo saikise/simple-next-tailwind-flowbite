19b7c468c17a2e3cccc78c4faa249656e8006880
sai kise <134133636+saikise@users.noreply.github.com>
2023-06-26 11:22:35 +0800

    remove beforeInteractive from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Script
-          strategy="beforeInteractive"
           src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"
         />
       </body>
